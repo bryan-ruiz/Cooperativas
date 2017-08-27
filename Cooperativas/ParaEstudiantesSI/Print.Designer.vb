ad6756fbf7bb6db8c6237a9ff4eb68747908935c
AaronRuizB <aaruiz89@yahoo.es>
2017-08-27 15:49:40 -0600

    changes to get back PDF reports

--- Cooperativas/ParaEstudiantesSI/Print.Designer.vb
+++ Cooperativas/ParaEstudiantesSI/Print.Designer.vb
@@ -22,59 +22,62 @@ Partial Class Print
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Print))
         Me.ButtonPrintAbrirPDF = New System.Windows.Forms.Button()
         Me.ButtonPrintSalir = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.AxAcroPDF1 = New AxAcroPDFLib.AxAcroPDF()
+        CType(Me.AxAcroPDF1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ButtonPrintAbrirPDF
         '
         Me.ButtonPrintAbrirPDF.BackgroundImage = Global.Cooperativas.My.Resources.Resources.btn2
-        Me.ButtonPrintAbrirPDF.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        resources.ApplyResources(Me.ButtonPrintAbrirPDF, "ButtonPrintAbrirPDF")
         Me.ButtonPrintAbrirPDF.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.ButtonPrintAbrirPDF.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonPrintAbrirPDF.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.ButtonPrintAbrirPDF.ForeColor = System.Drawing.Color.White
-        Me.ButtonPrintAbrirPDF.Location = New System.Drawing.Point(353, 5)
         Me.ButtonPrintAbrirPDF.Name = "ButtonPrintAbrirPDF"
-        Me.ButtonPrintAbrirPDF.Size = New System.Drawing.Size(157, 52)
-        Me.ButtonPrintAbrirPDF.TabIndex = 1
-        Me.ButtonPrintAbrirPDF.Text = "Abrir PDF"
         Me.ButtonPrintAbrirPDF.UseVisualStyleBackColor = True
         '
         'ButtonPrintSalir
         '
         Me.ButtonPrintSalir.BackgroundImage = Global.Cooperativas.My.Resources.Resources.btn2
-        Me.ButtonPrintSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        resources.ApplyResources(Me.ButtonPrintSalir, "ButtonPrintSalir")
         Me.ButtonPrintSalir.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.ButtonPrintSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonPrintSalir.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.ButtonPrintSalir.ForeColor = System.Drawing.Color.White
-        Me.ButtonPrintSalir.Location = New System.Drawing.Point(516, 5)
         Me.ButtonPrintSalir.Name = "ButtonPrintSalir"
-        Me.ButtonPrintSalir.Size = New System.Drawing.Size(157, 52)
-        Me.ButtonPrintSalir.TabIndex = 2
-        Me.ButtonPrintSalir.Text = "Salir"
         Me.ButtonPrintSalir.UseVisualStyleBackColor = True
         '
         'OpenFileDialog1
         '
         Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
+        'AxAcroPDF1
+        '
+        resources.ApplyResources(Me.AxAcroPDF1, "AxAcroPDF1")
+        Me.AxAcroPDF1.Name = "AxAcroPDF1"
+        Me.AxAcroPDF1.OcxState = CType(resources.GetObject("AxAcroPDF1.OcxState"), System.Windows.Forms.AxHost.State)
+        '
         'Print
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.CadetBlue
-        Me.ClientSize = New System.Drawing.Size(1047, 693)
-        Me.Controls.Add(Me.ButtonPrintSalir)
+        Me.BackColor = System.Drawing.Color.Gray
         Me.Controls.Add(Me.ButtonPrintAbrirPDF)
+        Me.Controls.Add(Me.ButtonPrintSalir)
+        Me.Controls.Add(Me.AxAcroPDF1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MinimizeBox = False
         Me.Name = "Print"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Print"
+        CType(Me.AxAcroPDF1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents ButtonPrintAbrirPDF As Button
     Friend WithEvents ButtonPrintSalir As Button
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents AxAcroPDF1 As AxAcroPDFLib.AxAcroPDF
     '-----------------------------------Friend WithEvents AxAcroPDF1 As AxAcroPDFLib.AxAcroPDF
 End Class
--- Cooperativas/ParaEstudiantesSI/Print.vb
+++ Cooperativas/ParaEstudiantesSI/Print.vb
@@ -5,11 +5,15 @@ Public Class Print
     Private Sub ButtonPrintAbrirPDF_Click(sender As Object, e As EventArgs) Handles ButtonPrintAbrirPDF.Click
         Me.OpenFileDialog1.FileName = String.Empty
         Me.OpenFileDialog1.ShowDialog()
-        '---------------Me.AxAcroPDF1.LoadFile(Me.OpenFileDialog1.FileName)
+        AxAcroPDF1.LoadFile(Me.OpenFileDialog1.FileName)
 
     End Sub
 
     Private Sub ButtonPrintSalir_Click(sender As Object, e As EventArgs) Handles ButtonPrintSalir.Click
         Me.Close()
+    End Sub
+
+    Private Sub Print_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
